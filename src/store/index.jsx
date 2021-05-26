275ba92574f6fcdbd1ca546bf452ba75d0b4178b
Mateusz Kopko <kopko.mateusz@gmail.com>
2021-05-26 22:49:35 +0200

    add default cypress files, add simple cypress test, add jest simple test and clean up config

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -9,11 +9,7 @@ const config = preloadState => {
   return createStore(
     reducers,
     preloadState,
-    compose(
-      applyMiddleware(thunk, logger),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    compose(applyMiddleware(thunk, logger))
   );
 };
 
